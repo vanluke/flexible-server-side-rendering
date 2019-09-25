b6b154a795f936675cc23b55406189b024be7be0
Lukasz Gonciarz <vanluke@op.pl>
2019-09-25 19:28:47 +0200

    feature(codebase) finish readme, clean up code

---
+++ certs/cert-gen.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#Required
+domain=$1
+commonname=$domain
+
+#Optional
+password=x
+
+if [ -z "$domain" ]
+then
+    echo "Domain is undefined."
+    echo "Useage $0 [common name]"
+
+    exit 99
+fi
+
+echo "Generating key request for $domain"
+
+#Generate a key
+openssl genrsa -des3 -passout pass:$password -out $domain.key 2048 -noout
+#Remove passphrase from the key. Comment the line out to keep the passphrase
+echo "Removing passphrase from key"
+openssl rsa -in $domain.key -passin pass:$password -out $domain.key
+
+#Create the request
+echo "Creating CSR"
+openssl req -new -key $domain.key -out $domain.csr -passin pass:$password \
+    -subj "/CN=$commonname"
+
+echo "---------------------------"
+echo "-----Below is your CSR-----"
+echo "---------------------------"
+echo
+cat $domain.csr
+
+echo
+echo "---------------------------"
+echo "-----Below is your Key-----"
+echo "---------------------------"
+echo
+cat $domain.key
--- readme.md
+++ readme.md
@@ -2,21 +2,23 @@
 
 ## Quick Start
 
-1. Clone repo
-2.
+1. setup .env file (see .env.example)
 
-```bash
-npm i / npm install
-cd my-app
-npm start
 ```
+# Http port for your server when using http protocol
+PORT=3000
+# Https port for your server when using http protocol
+PORT_HTTPS=3000
+# Specify if you want to use Http
+USE_HTTPS=true
+# Index file title
+APP_TITLE=NONAME
+```
+2. run ```npm install or npm i`
+optional if you decided to use ssl run ```npm run generate-certs```
+3. run ```npm run start:dev``` to start app in development mode
 
-Then open http://localhost:3000/ to see your app.
+## More scripts
+* ```build:production``` build production package
+* ```test``` run app tests (using jest)
 
-### `npm run build:production`
-
-Builds the app for production to the dist folder.
-
-### `npm test`
-
-Runs jest tests, supports rendering with enzyme
